e3cb27ed94c769f48d7b870ee01f61e6b2bc7c16
zhaizun <DMT1909216@xmu.edu.my>
2021-10-06 14:48:13 +0800

    First Commit

---
+++ LearnGitDemo/Shared/LearnGitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnGitDemoApp.swift
+//  Shared
+//
+//  Created by 翟尊 on 2021/10/6.
+//
+
+import SwiftUI
+
+@main
+struct LearnGitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
